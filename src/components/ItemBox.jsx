a054fc269078db72d432f4216a667e72b367e198
DivineDemon <supame123@gmail.com>
2023-11-17 04:32:50 +0500

    Cosmetic!

--- src/components/ItemBox.jsx
+++ src/components/ItemBox.jsx
@@ -1,7 +1,10 @@
+import { RiRobot2Line } from "react-icons/ri";
+
 const ItemBox = ({ item }) => {
   return (
     <div
       className={`${item.gradient} ${item.color} rounded-lg flex flex-col items-center justify-center space-y-4 p-5 cursor-pointer shadow-md hover:shadow-xl transition-transform transform hover:scale-105`}>
+      <RiRobot2Line className={`${item.color} w-10 h-10`} />
       <span className="text-md md:text-xl font-semibold underline underline-offset-4">
         {item.name}
       </span>
